204be449a37fedc2adefc354a5e6adf72e36bc08
rsbertoa90 <rsbertoa90@gmail.com>
2019-08-21 17:13:26 -0300

    phon

--- components/home/iframe-form.vue
+++ components/home/iframe-form.vue
@@ -3,9 +3,15 @@
         <h3>
             Quiero un presupuesto: completa el siguiente formulario
         </h3>
-        <span class="phone">
+        <span class="phone" v-if="$mq=='lg'">
             O escribinos al <span class="fab fa-whatsapp"></span> 11 2408 5256
         </span>
+
+        <a href="https://api.whatsapp.com/send?phone=5492920485028&text=Hola!%20te%20escribo%20desde%20la%20pagina%20de%20MoM%20Web" target="_blank" rel="noopener" class="phone">
+            O escribinos al <span class="fab fa-whatsapp"></span> 11 2408 5256
+        </a>
+
+        
         <div class="iframe-container">
                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfPOTyGDPT3gA1mHC5y__MbHfk5rVq9mN1VVRcapxIggSi7yw/viewform?embedded=true" :width="iframewidth" :height="iframeheight" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>                
         </div>
